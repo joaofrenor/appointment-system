80887a8d2839e0f533f2b265da753d660e002a35
João Felipe <37910709+joaofrenor@users.noreply.github.com>
2019-02-25 14:59:05 -0300

    Envio dos arquivos ao GitHub

---
+++ src/app/models/User.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcryptjs')
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.VIRTUAL,
+      avatar: DataTypes.STRING,
+      password_hash: DataTypes.STRING,
+      provider: DataTypes.BOOLEAN
+    },
+    {
+      hooks: {
+        beforeSave: async user => {
+          if (user.password) {
+            user.password_hash = await bcrypt.hash(user.password, 8)
+          }
+        }
+      }
+    }
+  )
+
+  User.prototype.checkPassword = async function (password) {
+    return bcrypt.compare(password, this.password_hash)
+  }
+
+  return User
+}
